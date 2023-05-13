bbb2d27717da895959934d3dcd16404c40c7e4c4
luk3yx <luk3yx@users.noreply.github.com>
2023-05-13 19:52:10 +1200

    Add worldedit.max_region_size setting

--- worldedit_commands/safe.lua
+++ worldedit_commands/safe.lua
@@ -2,12 +2,15 @@ local S = minetest.get_translator("worldedit_commands")
 
 --`count` is the number of nodes that would possibly be modified
 --`callback` is a callback to run when the user confirms
+local max_size = tonumber(minetest.settings:get("worldedit.max_region_size")) or 20000
+
 local function safe_region(name, count, callback)
-	if count < 20000 then
+	if count < max_size then
 		return callback()
 	end
 
-	worldedit.player_notify(name, "This operation would affect up to " .. count .. " nodes; you can only update 20,000 nodes at a time")
+	worldedit.player_notify(name, "This operation would affect up to " .. count .. " nodes; you can only update " ..
+		max_size .. " nodes at a time")
 end
 
 return safe_region
