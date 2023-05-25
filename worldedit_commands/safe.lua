7cccd9f434797652bc2373f5a547a44647d11285
luk3yx <luk3yx@users.noreply.github.com>
2023-05-25 16:30:59 +1200

    Hopefully fix crash and possible max size bypass

--- worldedit_commands/safe.lua
+++ worldedit_commands/safe.lua
@@ -7,6 +7,16 @@ local max_size = tonumber(minetest.settings:get("worldedit.max_area_size")) or 1
 local abs = math.abs
 local safe_region_callback = {}
 
+-- Like vector.equals but doesn't error if the positions are nil
+local function pos_eq(pos1, pos2)
+	-- If either position is nil then only return true if they're both nil
+	if pos1 == nil or pos2 == nil then
+		return pos1 == nil and pos2 == nil
+	end
+
+	return pos1.x == pos2.x and pos1.y == pos2.y and pos1.z == pos2.z
+end
+
 local function safe_region(name, count, callback, strict)
 	if count < max_nodes then
 		return callback()
@@ -20,7 +30,21 @@ local function safe_region(name, count, callback, strict)
 	end
 
 	--save callback to call later
-	safe_region_callback[name] = callback
+	local pos1, pos2 = worldedit.pos1[name], worldedit.pos2[name]
+	safe_region_callback[name] = function()
+		-- Replace the current positions with the ones from when the command
+		-- was initially executed.
+		local old_pos1, old_pos2 = worldedit.pos1[name], worldedit.pos2[name]
+		worldedit.pos1[name], worldedit.pos2[name] = table.copy(pos1), table.copy(pos2)
+
+		callback()
+
+		-- If the positions haven't been changed then restore the previous ones
+		if pos_eq(worldedit.pos1[name], pos1) and pos_eq(worldedit.pos2[name], pos2) then
+			worldedit.pos1[name], worldedit.pos2[name] = old_pos1, old_pos2
+		end
+	end
+
 	worldedit.player_notify(name, "WARNING: this operation could affect up to " .. count .. " nodes; type //y to continue or //n to cancel")
 end
 
